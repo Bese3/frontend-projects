2a499acb82da68ce9deb2da1ab80d2d53c43e919
besufikad <07yilmabese@gmail.com>
2024-08-15 12:09:45 +0300

    mods

--- FINET_TASK/src/server.py
+++ FINET_TASK/src/server.py
@@ -10,7 +10,7 @@ from flask import (
 import requests
 from requests.exceptions import ConnectionError
 from uuid import uuid4
-from util import population_rounder
+from util import population_rounder, get_countries_data
 
 
 app_views = Blueprint('app', __name__, url_prefix="/discover-ze-world")
@@ -26,91 +26,7 @@ def get_by_name(name):
     with the provided `name`. It then processes the response data
     and extracts relevant information about countries with the given name
     """
-    if not name:
-        return make_response(jsonify({'message': "empty name"}), 400)
-    res_list = []
-    try:
-        with requests.get(f'{third_url}name/{name}') as res:
-            if not res.ok:
-                return render_template("home.html", result=False,
-                                    message=res.json().get('message',
-                                                            'Not Found'),
-                                    status=res.json().get('status', 404))
-            res = res.json()
-            # print(res)
-            for country in res:
-                temp_obj = {}
-                try:
-                    temp_obj.update({'name': country['name']['common']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'official': country['name']['official']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'capital': country['capital'][0]})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'continent': country['region']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update(
-                        {
-                            'population': population_rounder(country['population'])
-                        }
-                        )
-                except (KeyError, ValueError, TypeError):
-                    pass
-                try:
-                    temp_obj.update({'subregion': country['subregion']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'location': country['maps']['googleMaps']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'timezone': country['timezones'][0]})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'flag': country['flags']['png']})
-                except KeyError:
-                    pass
-                try:
-                    for i in country['name']['nativeName'].keys():
-                        temp_obj.update(
-                        {
-                        'native': country['name']['nativeName'][i]['official']
-                        }
-                        )
-                        break
-                except KeyError:
-                    pass
-                try:
-                    for i in country['currencies'].keys():
-                        temp_obj.update(
-                        {
-                        'currency': country['currencies'][i]['name']
-                        }
-                        )
-                        break
-                except KeyError:
-                    pass
-                try:
-                    for i in country['languages'].keys():
-                        temp_obj.update({'language': country['languages'][i]})
-                        break
-                except KeyError:
-                    pass
-                temp_obj.update({'id': uuid4()})
-                res_list.append(temp_obj)
-    except ConnectionError:
-        return render_template("home.html", message="Connection Error")
-    return render_template("home.html", res_list=res_list, result=True)
+    return get_countries_data(name, "home.html")
 
 
 @app_views.route("/", methods=['GET'], strict_slashes=False)
@@ -131,109 +47,7 @@ def redirect_home():
 
 @app_views.route("/results/<name>", methods=['GET'], strict_slashes=False)
 def render_result(name):
-    if not name:
-        return make_response(jsonify({'message': "empty name"}), 400)
-    res_list = []
-    try:
-        with requests.get(f'{third_url}name/{name}') as res:
-            if not res.ok:
-                return render_template("home.html", result=False,
-                                    message=res.json().get('message',
-                                                            'Not Found'),
-                                    status=res.json().get('status', 404))
-            res = res.json()
-            # print(res)
-            for country in res:
-                temp_obj = {}
-                try:
-                    temp_obj.update({'name': country['name']['common']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'official': country['name']['official']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'capital': country['capital'][0]})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'continent': country['region']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update(
-                        {
-                            'population': population_rounder(country['population'])
-                        }
-                        )
-                except (KeyError, ValueError, TypeError):
-                    pass
-                try:
-                    temp_obj.update({'subregion': country['subregion']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'location': country['maps']['googleMaps']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'timezone': country['timezones'][0]})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'flag': country['flags']['png']})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'land': "Yes" if country['landlocked'] else "No"})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'latitude': country['latlng'][0]})
-                    temp_obj.update({'longitude': country['latlng'][1]})
-                except KeyError:
-                    pass
-                try:
-                    temp_obj.update({'area': f"{country['area']}sqr.KM"})
-                except KeyError:
-                    pass
-                try:
-                    for i in country['name']['nativeName'].keys():
-                        temp_obj.update(
-                        {
-                        'native': country['name']['nativeName'][i]['official']
-                        }
-                        )
-                        temp_obj.update(
-                        {
-                        'common': country['name']['nativeName'][i]['common']
-                        }
-                        )
-                        break
-                except KeyError:
-                    pass
-                try:
-                    for i in country['currencies'].keys():
-                        temp_obj.update(
-                        {
-                        'currency': country['currencies'][i]['name']
-                        }
-                        )
-                        break
-                except KeyError:
-                    pass
-                try:
-                    for i in country['languages'].keys():
-                        temp_obj.update({'language': country['languages'][i]})
-                        break
-                except KeyError:
-                    pass
-                temp_obj.update({'id': uuid4()})
-                res_list.append(temp_obj)
-    except ConnectionError:
-        return render_template("home.html", message="Connection Error")
-    return render_template('results.html', res_list=res_list)
+    return get_countries_data(name, "results.html")
 
 
 app.register_blueprint(app_views)
--- FINET_TASK/src/util.py
+++ FINET_TASK/src/util.py
@@ -1,3 +1,6 @@
+import requests
+from flask import render_template, make_response, jsonify
+from uuid import uuid4
 
 
 def population_rounder(population):
@@ -35,4 +38,116 @@ def population_rounder(population):
     return pop_str
 
 
-# def get_countries_data(name):
+third_url = 'https://restcountries.com/v3.1/'
+
+
+def get_countries_data(name, template):
+    if not name:
+        return make_response(jsonify({'message': "empty name"}), 400)
+    if not template:
+        return make_response(jsonify(
+            {'message': "something got wrong when rendering result"}), 400)
+    res_list = []
+    try:
+        with requests.get(f'{third_url}name/{name}') as res:
+            if not res.ok:
+                return render_template("home.html", result=False,
+                                       message=res.json().get('message',
+                                                              'Not Found'),
+                                       status=res.json().get('status', 404))
+            res = res.json()
+            # print(res)
+            for country in res:
+                temp_obj = {}
+                try:
+                    temp_obj.update({'name': country['name']['common']})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update({'official': country['name']['official']})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update({'capital': country['capital'][0]})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update({'continent': country['region']})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update(
+                     {
+                      'population': population_rounder(country['population'])
+                     }
+                        )
+                except (KeyError, ValueError, TypeError):
+                    pass
+                try:
+                    temp_obj.update({'subregion': country['subregion']})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update(
+                       {'location': country['maps']['googleMaps']})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update({'timezone': country['timezones'][0]})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update({'flag': country['flags']['png']})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update(
+                        {'land': "Yes" if country['landlocked'] else "No"})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update({'latitude': country['latlng'][0]})
+                    temp_obj.update({'longitude': country['latlng'][1]})
+                except KeyError:
+                    pass
+                try:
+                    temp_obj.update({'area': f"{country['area']}sqr.KM"})
+                except KeyError:
+                    pass
+                try:
+                    for i in country['name']['nativeName'].keys():
+                        native = country['name']['nativeName'][i]
+                        temp_obj.update(
+                         {
+                          'native': native['official']
+                         }
+                        )
+                        temp_obj.update(
+                         {
+                            'common': native['common']
+                         }
+                        )
+                        break
+                except KeyError:
+                    pass
+                try:
+                    for i in country['currencies'].keys():
+                        temp_obj.update(
+                         {
+                          'currency': country['currencies'][i]['name']
+                         }
+                        )
+                        break
+                except KeyError:
+                    pass
+                try:
+                    for i in country['languages'].keys():
+                        temp_obj.update({'language': country['languages'][i]})
+                        break
+                except KeyError:
+                    pass
+                temp_obj.update({'id': uuid4()})
+                res_list.append(temp_obj)
+    except ConnectionError:
+        return render_template("home.html", message="Connection Error")
+    return render_template(template, res_list=res_list, result=True)
