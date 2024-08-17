3527fa177d0bc6b79924c37b64eb8478979705fc
besufikad <07yilmabese@gmail.com>
2024-08-17 13:04:24 +0300

    enter key

--- FINET_TASK/src/static/js/main.js
+++ FINET_TASK/src/static/js/main.js
@@ -1,11 +1,29 @@
 
+function showFlashMessage(message) {
+    const flashMessage = document.getElementById('flash-message');
+    flashMessage.textContent = message;
+    flashMessage.classList.remove('hidden');
+    flashMessage.classList.add('show');
+
+    // Hide the message after 3 seconds
+    setTimeout(() => {
+        flashMessage.classList.remove('show');
+        flashMessage.classList.add('hidden');
+    }, 3000);
+}
+
+
 const myUrl = 'https://projects-vm2m.onrender.com/discover-ze-world/'
 
 const myButton = document.querySelector('.btn-search');
 const myEvent =  () => {
     let name = document.getElementById('search').value;
-    document.getElementById('search').value = '';
     console.log(name);
+    if (name == null || name == ""){
+        showFlashMessage("Enter a Valid Name");
+        return
+    }
+    document.getElementById('search').value = '';
     if (name) {
         window.location.href = `${myUrl}name/${name}`;
         console.log(location)
@@ -18,7 +36,14 @@ const myEvent =  () => {
 }
 myButton.addEventListener('click', myEvent);
 const inputField = document.getElementById('search');
-// inputField.addEventListener('keydown', myEvent);
+const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); //
+        console.log('Enter key pressed');
+        myEvent()
+    }
+};
+inputField.addEventListener('keydown', handleKeyDown);
 
 
 document.querySelectorAll('.search-item').forEach(item => {
